9183358bb00c3275be374f01b1ae90ba2a60a937
yakov103 <yaakov103@gmail.com>
2022-05-15 02:43:42 -0700

    added more tests

--- Test.cpp
+++ Test.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 using namespace ariel;
 
-TEST_CASE("OrgChart")
+TEST_CASE("Good case ")
 {
  
      OrgChart org;
@@ -64,10 +64,61 @@ TEST_CASE("OrgChart")
 
         
     }
-    int j = 0;
-    for (int i = 0; i < 100; i++)
-    {
-        CHECK(i == j);
-        j++; 
+
+    vector<string> collect_to_reverse ; 
+    collect_to_reverse.clear();
+       for (auto it = org.begin_level_order(); it != org.end_level_order(); it++) {
+            if (i == 0 ){
+                CHECK (*it == "root");
+                i++;
+                continue;
+            }
+            if (i <= 4){
+            token = *it;
+            collect_to_reverse.push_back(token);
+            forchild = "child"; 
+            forchild += to_string(i);
+            CHECK(token == forchild);
+            printf("%s\n", token.c_str());
+            i++;
+            continue;
+            }
+            if (5 <= i && i < 8){
+            token = *it;
+            collect_to_reverse.push_back(token);
+            forchild = "grandchild"; 
+            forchild += to_string(i%4);
+            printf("%s\n", token.c_str());
+            CHECK(token == forchild);
+            i++; 
+            continue;
+            }
+            if (9 <= i && i < 12){
+            token = *it;
+            collect_to_reverse.push_back(token);
+            forchild = "greatgrandchild";
+            forchild += to_string(i%4+1);
+            CHECK(token == forchild);
+            i++; 
+            continue;
+            }
+        
+       }
+        reverse(collect_to_reverse.begin(), collect_to_reverse.end());
+        for (auto it = org.begin_reverse_order(); it != org.reverse_order(); it++) {
+            CHECK (*it == collect_to_reverse.back()); // check if the reverse order is correct
+            collect_to_reverse.pop_back(); // remove the last element from the vector
+        }
     }
+TEST_CASE("Bad cases"){
+
+    OrgChart org; 
+    CHECK_THROWS(org.add_sub("root", "child")); // still no root 
+    CHECK_NOTHROW(org.add_root("root"));
+    CHECK_NOTHROW(org.add_sub("root", "child"));
+    CHECK_NOTHROW(org.add_sub("child", "grandchild"));
+    CHECK_NOTHROW(org.add_sub("grandchild", "greatgrandchild"));
+    CHECK_THROWS(org.add_sub("whatt", "cannot"));// no such parent
+    CHECK_THROWS(org.add_sub("bishbash", "child")); // no such parent
+
 }
--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -2,17 +2,10 @@
 using namespace std;
 namespace ariel
 {
-    OrgChart &OrgChart::add_root(string role){
-        root.role = role;
-        return *this;
-    }
+    OrgChart &OrgChart::add_root(string role){return *this;}
   
-    bool OrgChart::add_sub(Node &node, string &parent, string &child){
-        return true ; 
-    }
-      OrgChart &OrgChart::add_sub(string parent, string child) {
-        return *this;
-    }
+    bool OrgChart::add_sub(Node &node, string &parent, string &child){return true ; }
+    OrgChart &OrgChart::add_sub(string parent, string child) { return *this;}
     string *OrgChart::begin_level_order(){
         if (iter_vec_level.size() != 0){
            iter_vec_level.clear(); // clear the vector
@@ -28,47 +21,20 @@ namespace ariel
     }
     string *OrgChart::end_level_order(){return &iter_vec_level[iter_vec_level.size()];}
 
-    string *OrgChart::begin_reverse_order()
-    {
-        if (iter_vec_reverse.size() != 0){
-           iter_vec_reverse.clear(); // clear the vector
-        }
-        iter_vec_level.push_back(root.role);
-        fill_level_order(root);
-        return &iter_vec_level[0];
-
-    }
+    string *OrgChart::begin_reverse_order(){return &iter_vec_level[0];}
     void OrgChart::fill_reverse_order(Node &node)
     {
-         for (unsigned int i = 0; i < node.children.size(); i++)
-        {
-            iter_vec_level.push_back(node.children[i].role);
-        }
-        for (unsigned int i = 0; i < node.children.size(); i++)
-        {
-            fill_level_order(node.children[i]);
-        }
+
     }
     string *OrgChart::reverse_order(){return &iter_vec_level[iter_vec_level.size()];}
 
-    string *OrgChart::begin_preorder()
-    {
-        if (iter_vec_preorder.size() != 0){
-           iter_vec_preorder.clear(); // clear the vector
-        }
-        iter_vec_level.push_back(root.role);
-        return &iter_vec_level[0];
+    string *OrgChart::begin_preorder(){return &iter_vec_level[0];
     }
     void OrgChart::fill_preorder(Node &node)
     {}
     string *OrgChart::end_preorder(){return &iter_vec_level[iter_vec_level.size()]; }
     
     ostream &operator<<(ostream &COUT, OrgChart &org){
-        for (auto it = org.begin_preorder(); it != org.end_preorder(); ++it)
-        {
-            COUT << (*it); // print the string
-            COUT << endl;  // print the endline
-        }
         return COUT;
     }
 }
